c4d580300bdf8965f044a6e8d38492a36fe1cac3
Ljacoby0 <leylajacoby@gmail.com>
2020-07-14 15:46:15 -0400

    most recent changes

--- src/main.py
+++ src/main.py
@@ -7,19 +7,10 @@ data = {}
 def index():
     return render_template("index.html")
 
-@app.route("/compare", methods=['POST'])
-def compare():
-    info = request.form
-    data['address1'] = info['address']
-    data['zipcode1'] = info['zipcode']
-    return render_template("compare.html", address = data['address1'], zipcode = data['zipcode1'])
-
-@app.route("/results", methods=['POST'])
+@app.route("/results")
 def results():
-    info = request.form
-    data['address2'] = info['address2']
-    data['zipcode2'] = info['zipcode2']
-    return render_template("results.html", address1 = data['address1'], zipcode1 = data['zipcode1'], address2 = data['address2'], zipcode2 = data['zipcode2'])
+    print(request.args)
+    return render_template("results.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
