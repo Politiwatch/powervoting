0e1ca74b3abb7554634c534173b71a595c22173c
R. Miles McCain <development-machine@sendmiles.email>
2020-08-05 18:22:38 +0000

    Add basic history chart for house

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ def results():
     locations = [api.get_address_info(address, zipcode) for (address, zipcode) in addresses]
     elections = [api.get_elections(loc["state"], loc["district"]) for loc in locations]
     scores = [api.compute_scores(elecs) for elecs in elections]
-    info = list(zip(locations, scores))
+    info = list([[location, score] for location, score in zip(locations, scores)])
     #everything is included in 'info', since it zips locations (which includes input) and scores (which includes history)
     print(info)
     return render_template("results.html", locations=locations, scores=scores, info=info)
---
+++ src/static/base.js
@@ -0,0 +1,48 @@
+function distinct(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+function buildChart(electionSequences, labels, id) {
+    // console.log(electionSequences, labels);
+    let ctx = document.getElementById(id).getContext('2d');
+    let colors = chroma.scale(['red', 'navy']).mode('lch').colors(labels.length);
+    let chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: electionSequences.map(s => s.map(e => e.year)).flat().filter(distinct),
+            datasets: electionSequences.map((s, i) => {
+                return {
+                    label: labels[i],
+                    data: s.map(e => {
+                        return {
+                            x: e.year,
+                            y: (e.scores.closeness * 100).toFixed(2),
+                            label: `${labels[i]}: ${(e.scores.closeness * 100).toFixed(2)}% Closeness (${e.totalvotes.toLocaleString()} votes, ${e.candidates.length} candidates)`,
+                        }
+                    }),
+                    borderColor: colors[i],
+                    backgroundColor: colors[i],
+                    fill: false,
+                }
+            }),
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            tooltips: {
+                enabled: true,
+                mode: 'single',
+                callbacks: {
+                    label: function (tooltipItem, data) {
+                        return data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].label;
+                    },
+                }
+            },
+        }
+    });
+}
